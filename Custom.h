0da0425c8cdf1322baecf419c3155787060a7d8c
bloowb0und <bloowbound@gmail.com>
2021-04-15 18:14:58 +0300

    custom accessors

--- Custom.h
+++ Custom.h
@@ -1,16 +1,26 @@
 #pragma once
 #include "Event.h"
-class Custom :
-	public Event
+
+class Custom : public Event
 {
 	string description;
+
+	Date eventDate;
+	Time_ eventTime;
 
 public:
 
 	Custom();
-	Custom(string description);
+	Custom(string description, Date eventDate, Time_ eventTime);
 	~Custom();
 
-	void toString();
-};
+	Date getEventDate() const&;
+	Time_ getEventTime() const&;
+	void setEventDate(Date eventDate);
+	void setEventTime(Time_ eventTime);
 
+	string getDescription();
+	void setDescription(string description);
+
+	virtual void toString() override;
+};
